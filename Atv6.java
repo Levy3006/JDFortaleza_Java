f09eda39f79b30d7c1dfdef682f3649fc42cdac2
Levy Pinheiro <111375592+Levy3006@users.noreply.github.com>
2022-09-11 22:49:08 -0300

    Add files via upload
    Atividade 6 proposta na aula do dia 11/09/2022. professor André Wendel

---
+++ Atv6.java
@@ -0,0 +1,20 @@
+package atv6;
+import java.util.Scanner;
+public class Atv6 {
+    public static void main(String[] args) {
+        int i =1;
+        double soma=0;
+        do{
+            System.out.println("digite o valor da " + i + "º" + " nota: ");
+            Scanner valori = new Scanner(System.in);
+            int n1 = valori.nextInt();
+            i++;
+            soma+=n1;
+            if(i>4){
+                System.out.println("a media é:" + soma/4);
+            }
+
+        }while(i<=4);
+
+    }
+}
