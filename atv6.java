70558bb5a04f6e0791b4930c6326a449fc5ad622
Levy Pinheiro <111375592+Levy3006@users.noreply.github.com>
2022-09-17 12:07:30 -0300

    Update atv6.java

--- atv6.java
+++ atv6.java
@@ -2,18 +2,20 @@ package atv4;
 import java.util.Scanner;
 public class atv4 {
     public static void main(String[] args) {
+        
+            Scanner leitor = new Scanner(System.in);
 
             System.out.println("Quanto você ganha por hora?");
-            Scanner valorHora = new Scanner(System.in);
-            double vH = valorHora.nextDouble();
+            
+            double vH = leitor.nextDouble();
 
             System.out.println("quantas horas você trabalha por dia?");
-            Scanner horaDia = new Scanner(System.in);
-            double hD = horaDia.nextDouble();
+           
+            double hD = leitor.nextDouble();
 
-            System.out.println("quantos dias da semana você trabalha?");
-            Scanner diaSemana = new Scanner(System.in);
-            double dS = diaSemana.nextDouble();
+            System.out.println("quantos dias na semana você trabalha?");
+            
+            double dS = leitor.nextDouble();
 
             double salario_ = vH*hD*dS*4;
                     System.out.printf("seu salário é: %s",salario_);
